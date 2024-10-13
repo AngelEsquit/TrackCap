e3a1b417d46eff9821d2bcbe416fa1bec6857533
AngelEsquit <esq23221@uvg.edu.gt>
2024-10-12 18:28:08 -0600

    Creación homeScreen

---
+++ app/src/main/java/com/example/trackcap/ui/home/view/homeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.trackcap.ui.cards.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun homeScreen() {
+    Text(text = "Home Screen")
+}
