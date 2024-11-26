6be66de60d4950e8f98046d82566b008eeb5514e
JavierEspana23361 <131807933+JavierEspana23361@users.noreply.github.com>
2024-11-25 19:57:41 -0600

    commit

--- app/src/main/java/com/example/trackcap/MyApp.kt
+++ app/src/main/java/com/example/trackcap/MyApp.kt
@@ -2,45 +2,30 @@ package com.example.trackcap
 
 import android.app.Application
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.example.trackcap.database.AppDatabase
 import com.example.trackcap.ui.cards.repository.CardsRepository
 import com.example.trackcap.ui.gastos.repositories.GastosRepository
 import com.example.trackcap.ui.ingresos.repositories.IngresosRepository
+import com.example.trackcap.ui.invest.repository.InvestRepository
+
 
 class MyApp : Application() {
     lateinit var database: AppDatabase
-        private set
-
-    lateinit var gastosRepository: GastosRepository
-        private set
-
     lateinit var ingresosRepository: IngresosRepository
-        private set
-
+    lateinit var gastosRepository: GastosRepository
     lateinit var cardsRepository: CardsRepository
-        private set
+    lateinit var investRepository: InvestRepository
 
     override fun onCreate() {
         super.onCreate()
-
         database = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "trackcap-database"
-        ).setJournalMode(RoomDatabase.JournalMode.TRUNCATE)
-            .fallbackToDestructiveMigration()
-            .build()
+        ).build()
 
-        gastosRepository = GastosRepository(
-            database.gastoItemDao()
-        )
-
-        ingresosRepository = IngresosRepository(
-            database.ingresoItemDao()
-        )
-
-        cardsRepository = CardsRepository(
-            database.cardItemDao()
-        )
+        ingresosRepository = IngresosRepository(database.ingresoItemDao())
+        gastosRepository = GastosRepository(database.gastoItemDao())
+        cardsRepository = CardsRepository(database.cardItemDao())
+        investRepository = InvestRepository(database.activoItemDao())
     }
 }
